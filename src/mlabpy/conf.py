870e8d945ad5e921fb81facf0ad562b44e5c0c70
led02 <02@led-inc.eu>
2015-02-02 18:53:01 +0100

    missing config module

---
+++ src/mlabpy/conf.py
@@ -0,0 +1,9 @@
+"""
+MlabPy configuration.
+
+- ``DEBUG`` : Print debug output. (default: False)
+- ``LOADER_DUMP_TREE`` : Write AST to ``.ast`` file after parsing. (default: False)
+"""
+
+DEBUG = False
+LOADER_DUMP_TREE = False
